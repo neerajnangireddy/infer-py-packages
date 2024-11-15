ca9f3b9781ef228529f96fba98392e47a40a023f
NNK-KALI <neerajkumar18971@gmail.com>
2024-11-15 18:32:43 +0530

    Updated headers for curl in ipypk_install.sh

--- ipypk_install.sh
+++ ipypk_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl "https://raw.githubusercontent.com/neerajnangireddy/infer-py-packages/refs/heads/main/ipypk.py" -o ~/.local/bin/ipypk
+curl --proto "=https" --tlsv.12 -H 'Cache-Control: no-cache' -f "https://raw.githubusercontent.com/neerajnangireddy/infer-py-packages/refs/heads/main/ipypk.py" -o ~/.local/bin/ipypk
 chmod +x ~/.local/bin/ipypk
 
